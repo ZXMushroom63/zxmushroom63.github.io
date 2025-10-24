3e8030d95cd24330e43e3ca2adff1760957215f4
ZXMushroom63 <zxmushroom63@gmail.com>
2025-10-24 21:12:26 +0800

    finish verlet integration physics engine

---
+++ physics.js
@@ -0,0 +1,122 @@
+// custom made physics engine based on verlet integration
+
+const world = {
+    left: 0,
+    top: 0,
+    right: 2000,
+    bottom: 2000
+}
+const noise = 0.01;
+const airResistance = 0.7;
+const entities = [
+    { x: 1000, y: 1000, preX: 1000, preY: 1000, radius: 25, id: "mouse" },
+    { x: 606, y: 600, preX: 901, preY: 900, radius: 125, id: "" },
+    { x: 602, y: 600, preX: 902, preY: 900, radius: 125, id: "" },
+    { x: 607, y: 600, preX: 903, preY: 900, radius: 125, id: "" },
+]
+const gravity = { x: 0, y: 15 };
+const restitution = 0.9;
+function clamp(x, min, max) {
+    return Math.min(max, Math.max(min, x));
+}
+const timeScale = 1;
+function worldTick(deltaTime) {
+    deltaTime *= timeScale;
+
+    const minX = world.left;
+    const minY = world.top;
+    const maxX = world.right;
+    const maxY = world.bottom;
+
+    entities[0].x = entities[0].preX = mouse.nx * devicePixelRatio;
+    entities[0].y = entities[0].preY = mouse.ny * devicePixelRatio;
+    if (mouse.nx < 0) {
+        entities[0].radius = 0;
+    } else {
+        entities[0].radius = 25;
+    }
+
+    const velMult = 1 - (airResistance * deltaTime);
+
+
+
+    // collision pairs
+    for (let iter = 0; iter < 1; iter++) {
+        for (let i = 0; i < entities.length; i++) {
+            for (let j = 0; j < entities.length; j++) {
+                if (i === j) {
+                    continue;
+                }
+                const a = entities[i];
+                const b = entities[j];
+                if (a.id === "mouse") {
+                    continue;
+                }
+
+                const dx = b.x - a.x;
+                const dy = b.y - a.y;
+
+                const dist = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+                const combinedRadius = a.radius + b.radius;
+                if (combinedRadius > dist) {
+                    const depth = (dist - combinedRadius) / (b.id === "mouse" ? 16 : 2);
+                    const cos = dx / dist;
+                    const sin = dy / dist;
+                    a.x += depth * cos;
+                    a.y += depth * sin;
+                    a.preY -= depth * cos * restitution;
+                    a.preY -= depth * sin * restitution;
+                }
+            }
+        }
+    }
+
+    entities.forEach(entity => {
+        const r = entity.radius;
+
+        entity.x ||= 0;
+        entity.y ||= 0;
+
+        entity.preX ||= 0;
+        entity.preY ||= 0;
+
+        const currentX = entity.x;
+        const currentY = entity.y;
+
+        const nextX = currentX + (currentX - entity.preX) * velMult + (gravity.x * deltaTime);
+        const nextY = currentY + (currentY - entity.preY) * velMult + (gravity.y * deltaTime);
+
+        entity.preX = currentX;
+        entity.preY = currentY;
+
+        let finalX = nextX + (Math.random() - 0.5) * noise;
+        let finalY = nextY + (Math.random() - 0.5) * noise;
+
+        const minBoundX = minX + r;
+        const maxBoundX = maxX - r;
+
+        if (finalX < minBoundX) {
+            finalX = minBoundX;
+            entity.preX = currentX - (minBoundX - nextX) * restitution;
+
+        } else if (finalX > maxBoundX) {
+            finalX = maxBoundX;
+            entity.preX = currentX - (maxBoundX - nextX) * restitution;
+        }
+
+        const minBoundY = minY + r;
+        const maxBoundY = maxY - r;
+
+        if (finalY < minBoundY) {
+            finalY = minBoundY;
+            entity.preY = currentY - (minBoundY - nextY) * restitution;
+
+        } else if (finalY > maxBoundY) {
+            finalY = maxBoundY;
+            entity.preY = currentY - (maxBoundY - nextY) * restitution;
+        }
+
+        entity.x = finalX;
+        entity.y = finalY;
+    });
+}
--- scenes/projects.js
+++ scenes/projects.js
@@ -1,3 +1,23 @@
-renderer["projects"] = function (mainCtx, renderLeft, renderTop, renderRight, renderBottom, wordWrapText, myText) {
-    descPos += wordWrapText(renderLeft, renderTop + 36, "WIP", 36, renderRight);
+renderer["projects"] = function (mainCtx, renderLeft, renderTop, renderRight, renderBottom, wordWrapText, myText, deltaTime) {
+    mainCtx.fillStyle = `white`;
+    mainCtx.textAlign = "left";
+    mainCtx.font = "36px monospace";
+    wordWrapText(renderLeft, renderTop + 36, "WIP", 36, renderRight);
+    world.left = renderLeft;
+    world.right = renderRight;
+    world.top = renderTop;
+    world.bottom = renderBottom;
+    worldTick(deltaTime);
+    mainCtx.strokeStyle = "";
+    mainCtx.shadowColor = "transparent";
+    mainCtx.shadowBlur = 0;
+    mainCtx.fillStyle = `rgba(255,255,255,0.1)`;
+    entities.forEach(ent => {
+        if (ent.id === "mouse") {
+            return;
+        }
+        mainCtx.beginPath();
+        mainCtx.arc(ent.x, ent.y, ent.radius, 0, Math.PI * 2);
+        mainCtx.fill();
+    });
 }
