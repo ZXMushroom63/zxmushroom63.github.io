14dccbc014b8725206a99e52ccbd7469bde6e394
ZXMushroom63 <zxmushroom63@gmail.com>
2025-10-27 18:58:22 +0800

    disable glowy effect

--- scenes/projects.js
+++ scenes/projects.js
@@ -1,14 +1,28 @@
 renderer["projects"] = function (mainCtx, renderLeft, renderTop, renderRight, renderBottom, wordWrapText, myText, deltaTime) {
     const now = Date.now();
+
+    const dpr = devicePixelRatio;
+    const radius = vw(0.07);
+    const diam = radius * 2;
+
+    const defaultWidth = 1 * dpr;
+    const inspectedWidth = 2 * dpr;
+    const grabbedWidth = 1.5 * dpr;
+
+    const defaultStyle = "rgba(255,255,255,0.4)";
+    const inspectedStyle = "rgba(255,255,255,1)";
+    const grabbedOpacity = 0.7 + (Math.sin(now / 200) * 0.3);
+    const grabbedStyle = `rgba(255,255,255,${grabbedOpacity})`;
+
     mainCtx.fillStyle = `white`;
     mainCtx.textAlign = "left";
-    mainCtx.lineWidth = 1 * devicePixelRatio;
     mainCtx.font = "36px monospace";
     world.left = renderLeft;
     world.right = renderRight;
     world.top = renderTop;
     world.bottom = renderBottom;
     worldTick(deltaTime);
+
     mainCtx.strokeStyle = "rgba(255,255,255,0.5)";
     mainCtx.shadowColor = "rgba(255,255,255,0.5)";
     mainCtx.shadowBlur = 15;
@@ -17,55 +31,60 @@ renderer["projects"] = function (mainCtx, renderLeft, renderTop, renderRight, re
     mainCtx.moveTo(renderLeft, renderBottom - 1);
     mainCtx.lineTo(renderRight, renderBottom - 1);
     mainCtx.stroke();
+    
+    const len = entities.length;
+    for (let i = 0; i < len; i++) {
+        const ent = entities[i];
+        
+        ent.radius = radius;
 
-    mainCtx.lineWidth = 1 * devicePixelRatio;
+        const x = ent.x;
+        const y = ent.y;
 
-    entities.forEach(ent => {
-        ent.radius = vw(0.07);
-
-        switch (ent) {
-            case inspectedEntity:
-                mainCtx.lineWidth = 2 * devicePixelRatio;
-                mainCtx.strokeStyle = "rgba(255,255,255,1)";
-                break;
-            case grabbedEntity:
-                mainCtx.lineWidth = 1.5 * devicePixelRatio;
-                mainCtx.strokeStyle = `rgba(255,255,255,${0.7 + (Math.sin(now / 200) * 0.3)})`;
-                break;
-            default:
-                mainCtx.lineWidth = 1 * devicePixelRatio;
-                mainCtx.strokeStyle = "rgba(255,255,255,0.4)";
-                break;
+        if (ent === inspectedEntity) {
+            mainCtx.lineWidth = inspectedWidth;
+            mainCtx.strokeStyle = inspectedStyle;
+        } else if (ent === grabbedEntity) {
+            mainCtx.lineWidth = grabbedWidth;
+            mainCtx.strokeStyle = grabbedStyle;
+        } else {
+            mainCtx.lineWidth = defaultWidth;
+            mainCtx.strokeStyle = defaultStyle;
         }
-
 
         mainCtx.save();
         mainCtx.beginPath();
-        mainCtx.arc(ent.x, ent.y - 1, ent.radius, 0, Math.PI * 2);
+        mainCtx.arc(x, y - 1, radius, 0, Math.PI * 2);
         mainCtx.clip();
         mainCtx.shadowColor = ``;
         mainCtx.shadowBlur = 0;
         mainCtx.fillStyle = "black";
-        mainCtx.fillRect(ent.x - ent.radius, ent.y - ent.radius - 1, ent.radius * 2, ent.radius * 2);
-        mainCtx.drawImage(assets[ent.tex](), ent.x - ent.radius, ent.y - ent.radius - 1, ent.radius * 2, ent.radius * 2);
+        mainCtx.fillRect(x - radius, y - radius - 1, diam, diam);
+        mainCtx.drawImage(assets[ent.tex](), x - radius, y - radius - 1, diam, diam);
         mainCtx.restore();
-        mainCtx.shadowColor = `rgba(255,255,255,${clamp(Math.abs(ent.x - ent.preX) + Math.abs(ent.y - ent.preY) / 150, 0.15, 1)})`;
-        mainCtx.beginPath();
-        mainCtx.arc(ent.x, ent.y, ent.radius, 0, Math.PI * 2);
-        mainCtx.stroke();
-    });
 
-    if (grabbedEntity || inspectedEntity) {
-        const targ = (grabbedEntity ?? inspectedEntity);
+        //const shadowAlpha = clamp(Math.abs(x - ent.preX) + Math.abs(y - ent.preY) / 150, 0.15, 1);
+        //mainCtx.shadowColor = `rgba(255,255,255,${shadowAlpha})`;
+        
+        mainCtx.beginPath();
+        mainCtx.arc(x, y, radius, 0, Math.PI * 2);
+        mainCtx.stroke();
+    }
+
+    const targ = (grabbedEntity ?? inspectedEntity);
+    if (targ) {
         mainCtx.textAlign = "center";
         mainCtx.fillStyle = "rgba(0,0,0,0.75)";
         const metrics = mainCtx.measureText(targ.name);
+
         mainCtx.shadowColor = ``;
         mainCtx.shadowBlur = 0;
-        mainCtx.fillRect(targ.x - (metrics.width / 2) - 6, targ.y + targ.radius - 6 + (18/2), metrics.width + 12, 36 + 6);
+        
+        mainCtx.fillRect(targ.x - (metrics.width / 2) - 6, targ.y + radius - 6 + (18/2), metrics.width + 12, 36 + 6);
         mainCtx.fillStyle = "white";
-        mainCtx.fillText(targ.name, targ.x, targ.y + targ.radius + 36);
+        mainCtx.fillText(targ.name, targ.x, targ.y + radius + 36);
     }
+    
     mainCtx.shadowColor = ``;
     mainCtx.shadowBlur = 0;
     mainCtx.textAlign = "left";
