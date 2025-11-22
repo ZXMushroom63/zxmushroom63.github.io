6933d9ab1083a4229dd01b9bc8ab2a9c8c9cf128
ZXMushroom63 <zxmushroom63@gmail.com>
2025-11-22 19:41:34 +0800

    finish pfp gallery

--- scripts/scenes/pfps.js
+++ scripts/scenes/pfps.js
@@ -4,26 +4,31 @@ renderer["pfps"] = function (mainCtx, renderLeft, renderTop, renderRight, render
     mainCtx.font = px(24)+"px monospace";
     if (htmlcontent.writeEvent) {
         displayText(`
-            <img
+    <div class="pfp"><img
       class="pfp"
       src="public/pfps/modern.png"
       id="pfp_modern"
-    />
-    <img
+    /><span>Modern</span></div>
+    <div class="pfp"><img
       class="pfp"
       src="public/pfps/rain.png"
       id="pfp_rain"
-    />
-    <img
+    /><span>Rainy</span></div>
+    <div class="pfp"><img
       class="pfp"
       src="public/pfps/dreamy.png"
       id="pfp_dreamy"
-    />
-    <img
+    /><span>Dreamy</span></div>
+    <div class="pfp"><img
       class="pfp"
       src="public/pfps/indistinct.png"
       id="pfp_indistinct"
-    />
+    /><span>Indistinct</span></div>
+    <div class="pfp"><img
+      class="pfp"
+      src="public/pfps/old.png"
+      id="pfp_old"
+    /><span>Old</span></div>
             `, "Profile Pictures", true, true);
         htmlcontent.writeEvent = false;
     }
