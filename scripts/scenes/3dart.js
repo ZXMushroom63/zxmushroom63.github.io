0019ca6fb4c78988c11fa43681075b689b1945ec
ZXMushroom63 <zxmushroom63@gmail.com>
2025-11-25 16:42:28 +0800

    add 3d renders save button

--- scripts/scenes/3dart.js
+++ scripts/scenes/3dart.js
@@ -6,3 +6,19 @@ renderer["fn_3dart"] = function () {
         overrideImage = null;
     }
 }
+addEventListener("load", () => {
+    const save3d = document.querySelector("#save3d");
+    save3d.addEventListener("click", () => {
+        if (overrideImage) {
+            const dlTarget = document.createElement("a");
+            dlTarget.download = overrideImage.endsWith("_w") ? overrideImage.replace("_w", "") + ".webp" : overrideImage + ".png";
+            dlTarget.href = assets[overrideImage]().src;
+            dlTarget.click();
+        } else {
+            const dlTarget = document.createElement("a");
+            dlTarget.download = "output_file.webm";
+            dlTarget.href = "images/output_file.webm";
+            dlTarget.click();
+        }
+    });
+});
