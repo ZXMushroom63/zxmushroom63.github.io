19adfa210c7c42abec7eb044da8b2e09e29cd452
ZXMushroom63 <zxmushroom63@gmail.com>
2025-11-25 16:30:33 +0800

    switch 3d renders to a toggle method

--- scripts/scenes/3dart.js
+++ scripts/scenes/3dart.js
@@ -1,4 +1,8 @@
 renderer["fn_3dart"] = function () {
-    const Pool = [null, ...Object.keys(assets).filter(x => x.startsWith("r_"))];
-    overrideImage = Pool[Math.floor(Math.random() * Pool.length)];
+    const Pool = Object.keys(assets).filter(x => x.startsWith("r_"));
+    if (!overrideImage) {
+        overrideImage = Pool[Math.floor(Math.random() * Pool.length)];
+    } else {
+        overrideImage = null;
+    }
 }
